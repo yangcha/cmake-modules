478877eceb94e4036015e537d175e94a7b064d6c
Changjiang Yang <yangcha@leidos.com>
2015-02-27 16:23:35 -0500

    Fusing Google Test and make it more portable

--- modules/gtest.cmake
+++ modules/gtest.cmake
@@ -19,23 +19,19 @@
 # Distributed under the BSD License: 
 # http://choosealicense.com/licenses/bsd-2-clause/
 
-set(GTEST_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
-set(GTEST_ROOT "${GTEST_CMAKE_DIR}/../contrib/gtest-1.7.0" CACHE PATH "Root path to the Google Test.")
-
-if(MSVC)
-	set( gtest_force_shared_crt ON CACHE BOOL "Use /MD instead of /MT for MSVC" )
-endif()
-
-add_subdirectory(${GTEST_ROOT})
+set(GTEST_ROOT "${CMAKE_CURRENT_LIST_DIR}" CACHE PATH "Root path to the Google Test.")
 
 #make test or ctest
 enable_testing()
 
-include_directories(SYSTEM ${GTEST_ROOT}/include)
+include_directories(SYSTEM ${GTEST_ROOT})
 
 function(add_gtest target)
-	add_executable(${target} ${ARGN})
-	target_link_libraries(${target} gtest gtest_main)
+	add_definitions(-DGTEST_HAS_PTHREAD=0)
+	add_executable(${target} ${ARGN}
+		${GTEST_ROOT}/gtest_main.cc
+		${GTEST_ROOT}/gtest/gtest-all.cc
+		)
 	add_test(${target} ${target})
 endfunction()
 
